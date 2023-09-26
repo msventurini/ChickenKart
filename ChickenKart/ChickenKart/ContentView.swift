fda2ff0e4fd9a7b9abc1f6d7dd53338722372544
msventurini <42521920+msventurini@users.noreply.github.com>
2023-09-26 12:49:18 -0300

    Trying the 3d illusion!

--- ChickenKart/ChickenKart/ContentView.swift
+++ ChickenKart/ChickenKart/ContentView.swift
@@ -8,29 +8,114 @@
 import SwiftUI
 import SpriteKit
 
+
+
+struct rotationState {
+    
+}
+
 struct ContentView: View {
+    
+    @State var angleDeg: Double = 0
+    @State var offSetX: Double = 0
+    @State var offSetY: Double = 0
+    
+    @State var offSetXValue: Double = 0
+    @State var offSetYValue: Double = 15
+    
+//    @State
+
+    
     var body: some View {
         
-        var scene: GameScene {
-            let scene = GameScene()
-            scene.scaleMode = .aspectFill
-            return scene
-        }
+//        var scene: GameScene {
+//            let scene = GameScene()
+//            scene.scaleMode = .aspectFill
+//            return scene
+//        }
+//
+//        ZStack(alignment: .bottom) {
+//            SpriteView(scene: scene)
+//
+//            VStack {
+//
+//                MotionBasicsView()
+//
+//            }
+//            .frame(height: 100)
+//
+//
+//
+//        }
+//        .edgesIgnoringSafeArea(.all)
         
-        ZStack(alignment: .bottom) {
-            SpriteView(scene: scene)
+        ZStack {
+            Image("circuit")
+                .offset(x: offSetX, y: offSetY)
+                .scaleEffect(10)
             
-            VStack {
+                .rotation3DEffect(Angle(degrees: angleDeg), axis: (x: 0.0, y: 0.0, z: 1.0))
+                .rotation3DEffect(Angle(degrees: 85), axis: (x: 1.0, y: 0.0, z: 0.0))
+            HStack {
+                Rectangle()
+                    .overlay(content: {
+                        Text("left")
+                            .colorInvert()
+                    })
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            angleDeg -= 15
+                        }
+                    }
                 
-                MotionBasicsView()
+                
+                Rectangle()
+                    .overlay(content: {
+                        Text("up")
+                            .colorInvert()
+                    })
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            offSetY += 15 * cos(angleDeg)
+//                            offSetX -= 15 * sin(angleDeg)
+                        }
+                    }
+                
+                Rectangle()
+                    .overlay(content: {
+                        Text("down")
+                            .colorInvert()
+                    })
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            offSetY -= 15
+                        }
+                    }
+                
+                
+                
+                Rectangle()
+                    .overlay(content: {
+                        Text("right")
+                            .colorInvert()
+                    })
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            angleDeg += 15
+                        }
+                    }
                 
             }
-            .frame(height: 100)
-            
-            
+            .offset(y: 100)
             
         }
-        .edgesIgnoringSafeArea(.all)
+        
+        
+        
     }
 }
 
