53af4772b5aa6099aa1a34948390dc8bd45e47d6
msventurini <42521920+msventurini@users.noreply.github.com>
2023-09-25 22:51:44 -0300

    Setting up the update routine.

---
+++ ChickenKart/ChickenKart/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  ChickenKart
+//
+//  Created by Matheus Silveira Venturini on 25/09/23.
+//
+
+import SpriteKit
+import SwiftUI
+import GameController
+
+class GameScene: SKScene {
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+    }
+    
+    override func didMove(to view: SKView) {
+        self.removeAllChildren()
+    }
+    
+}
