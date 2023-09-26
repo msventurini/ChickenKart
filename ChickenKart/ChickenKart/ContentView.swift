9840c5df8c89b5c3882fc2fdaa875fa0f0bbbd91
msventurini <42521920+msventurini@users.noreply.github.com>
2023-09-26 14:24:29 -0300

    Trying the 3d illusion!

--- ChickenKart/ChickenKart/ContentView.swift
+++ ChickenKart/ChickenKart/ContentView.swift
@@ -10,9 +10,6 @@ import SpriteKit
 
 
 
-struct rotationState {
-    
-}
 
 struct ContentView: View {
     
@@ -28,91 +25,27 @@ struct ContentView: View {
     
     var body: some View {
         
-//        var scene: GameScene {
-//            let scene = GameScene()
-//            scene.scaleMode = .aspectFill
-//            return scene
-//        }
-//
-//        ZStack(alignment: .bottom) {
-//            SpriteView(scene: scene)
-//
-//            VStack {
-//
-//                MotionBasicsView()
-//
-//            }
-//            .frame(height: 100)
-//
-//
-//
-//        }
-//        .edgesIgnoringSafeArea(.all)
-        
-        ZStack {
-            Image("circuit")
-                .offset(x: offSetX, y: offSetY)
-                .scaleEffect(10)
-            
-                .rotation3DEffect(Angle(degrees: angleDeg), axis: (x: 0.0, y: 0.0, z: 1.0))
-                .rotation3DEffect(Angle(degrees: 85), axis: (x: 1.0, y: 0.0, z: 0.0))
-            HStack {
-                Rectangle()
-                    .overlay(content: {
-                        Text("left")
-                            .colorInvert()
-                    })
-                    .frame(width: 100, height: 100)
-                    .onTapGesture {
-                        withAnimation {
-                            angleDeg -= 15
-                        }
-                    }
-                
-                
-                Rectangle()
-                    .overlay(content: {
-                        Text("up")
-                            .colorInvert()
-                    })
-                    .frame(width: 100, height: 100)
-                    .onTapGesture {
-                        withAnimation {
-                            offSetY += 15 * cos(angleDeg)
-//                            offSetX -= 15 * sin(angleDeg)
-                        }
-                    }
-                
-                Rectangle()
-                    .overlay(content: {
-                        Text("down")
-                            .colorInvert()
-                    })
-                    .frame(width: 100, height: 100)
-                    .onTapGesture {
-                        withAnimation {
-                            offSetY -= 15
-                        }
-                    }
-                
-                
-                
-                Rectangle()
-                    .overlay(content: {
-                        Text("right")
-                            .colorInvert()
-                    })
-                    .frame(width: 100, height: 100)
-                    .onTapGesture {
-                        withAnimation {
-                            angleDeg += 15
-                        }
-                    }
-                
-            }
-            .offset(y: 100)
-            
+        var scene: GameScene {
+            let scene = GameScene()
+            scene.scaleMode = .aspectFill
+            return scene
         }
+
+        ZStack(alignment: .bottom) {
+            SpriteView(scene: scene)
+
+            VStack {
+
+                MotionBasicsView()
+
+            }
+            .frame(height: 100)
+
+
+
+        }
+        .edgesIgnoringSafeArea(.all)
+        
         
         
         
