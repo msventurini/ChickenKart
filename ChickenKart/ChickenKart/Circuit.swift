f5282f66556581109e65b1719f9cebcac816d445
msventurini <42521920+msventurini@users.noreply.github.com>
2023-09-26 15:14:54 -0300

    Trying the 3d illusion!

---
+++ ChickenKart/ChickenKart/Circuit.swift
@@ -0,0 +1,25 @@
+//
+//  Circuit.swift
+//  ChickenKart
+//
+//  Created by Matheus Silveira Venturini on 26/09/23.
+//
+
+//import SpriteKit
+//
+//class Circuit: SKSpriteNode {
+//    
+//    init() {
+//        
+//        super.init(fileNamed: "circuit")!
+//        
+//        
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//}
+//
+
