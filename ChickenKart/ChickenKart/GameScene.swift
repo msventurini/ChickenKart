5ffaa661f3f8dc3743190d46ca3398127d54acdf
msventurini <42521920+msventurini@users.noreply.github.com>
2023-09-27 03:16:09 -0300

    Added game controller for debug purposes.

--- ChickenKart/ChickenKart/GameScene.swift
+++ ChickenKart/ChickenKart/GameScene.swift
@@ -9,107 +9,9 @@ import SpriteKit
 import SwiftUI
 import GameController
 
-enum Direction {
-    case w
-    case nw
-    case n
-    case no
-    case o
-    
-    case so
-    case s
-    case sw
-    
-    
-    var turnRight: Direction {
-        switch self {
-        case .w:
-            return .nw
-        case .nw:
-            return .n
-        case .n:
-            return .no
-        case .no:
-            return .o
-        case .o:
-            return .so
-        case .so:
-            return .s
-        case .s:
-            return .sw
-        case .sw:
-            return .w
-        }
-    }
-    
-    var turnLeft: Direction {
-        switch self {
-        case .w:
-            return .sw
-        case .nw:
-            return .w
-        case .n:
-            return .nw
-        case .no:
-            return .n
-        case .o:
-            return .no
-        case .so:
-            return .o
-        case .s:
-            return .so
-        case .sw:
-            return .s
-        }
-    }
-    
-    var offsetX: Double {
-        switch self {
-        case .w:
-            return -1
-        case .nw:
-            return -0.7
-        case .n:
-            return 0
-        case .no:
-            return 0.7
-        case .o:
-            return 1
-        case .so:
-            return 0.7
-        case .s:
-            return 0
-        case .sw:
-            return -0.7
-        }
-    }
-    
-    var offsetY: Double {
-        switch self {
-        case .w:
-            return 0
-        case .nw:
-            return 0.7
-        case .n:
-            return 1
-        case .no:
-            return 0.7
-        case .o:
-            return 0
-        case .so:
-            return -0.7
-        case .s:
-            return -1
-        case .sw:
-            return -0.7
-        }
-    }
-    
-}
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    //    let circuitTexture = SKTexture(imageNamed: "circuit")
     
     var player = SKTransformNode()
     
@@ -120,38 +22,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var vectorGravity = CGVector(dx: 0, dy: -2.0)
     
     var virtualController: GCVirtualController?
-    
-    var direction: Direction = .n
-    
+        
     var angle: Double = 0
     
     
     override func sceneDidLoad() {
         super.sceneDidLoad()
-        
-        
-        //        self.scene?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        //        self.circuitNode.scene?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        //        self.circuitNode.position = CGPoint(x: -size.width/2, y: -size.height/2)
-        //        backgroundColor = .purple
+
     }
-    
-    //    override func update(_ currentTime: TimeInterval) {
-    ////        circuitNode.zRotation += 0.1
-    //    }
+
     
     override func didMove(to view: SKView) {
-        //        self.removeAllChildren()
         
         self.addChild(player)
         
         self.physicsWorld.gravity = CGVectorMake(0.0, 0.0)
         self.physicsWorld.contactDelegate = self
         
-        
-        
         setupVirtualController()
-        
         
         player.position = CGPoint(x: frame.midX, y: frame.midY)
         
@@ -159,23 +47,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         circuitNode.anchorPoint = CGPoint(x: 0, y: 0)
         circuitNode.position = CGPoint(x: -20, y: -20)
         
-        
         let ground = SKSpriteNode(color: .brown, size: .init(width: 160, height: 10))
         
         ground.anchorPoint = CGPoint(x: 0, y: 0)
         ground.position = CGPoint(x: circuitNode.frame.minX + 20, y: circuitNode.frame.minY + 20)
         ground.name = "ground"
-        
-        
-        
+
         circuitNode.addChild(ground)
         
         let ceil = SKSpriteNode(color: .brown, size: .init(width: 160, height: 10))
         ceil.anchorPoint = CGPoint(x: 0, y: 0)
         ceil.position = CGPoint(x: circuitNode.frame.minX + 20, y: circuitNode.frame.maxY + 20)
         ceil.name = "ceil"
-        
-        
+       
         circuitNode.addChild(ceil)
         
         let wall1 = SKSpriteNode(color: .brown, size: .init(width: 10, height: 144))
@@ -196,8 +80,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         center.anchorPoint = CGPoint(x: 0, y: 0)
         center.position = CGPoint(x: circuitNode.frame.midX + 20, y: circuitNode.frame.midY - 20)
         center.name = "center"
-        
-        
+
         circuitNode.name = "circuit"
         
         circuitNode.addChild(center)
